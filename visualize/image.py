550727945dcfd10b69f18185909210be7798e518
Apogentus <v.v.kitov@yandex.ru>
2019-05-10 19:31:57 +0300

    Added show_images function

--- visualize/image.py
+++ visualize/image.py
@@ -1,6 +1,8 @@
 import numpy as np
-from pylab import plt
 from IPython import display
+from pylab import *
+import os
+from PIL import Image
 
 def dynamic_img_show(img,title_str='',fig_size=[14,8],hide_axes=True):
     '''Show image <img>. If called repeatedly within a cycle will dynamically redraw image.
@@ -20,3 +22,26 @@ def dynamic_img_show(img,title_str='',fig_size=[14,8],hide_axes=True):
     plt.gcf().set_size_inches(fig_size)
     display.display(plt.gcf())
     display.clear_output(wait=True)   
+
+
+def show_image(pil_image, title_str):
+    '''show PIL image [pil_image], giving a title if [title_str] is provided'''
+    imshow(pil_image); xticks([]); yticks([]); 
+    if title_str:
+        title(title_str); 
+
+
+def show_images(path, K=3, figsize=(20,20)):
+    '''Display all images in [path] row by row. In each row [K] images are displayed.
+    [figsize] controls size of figure containing all images in one row.'''
+    
+    figure(figsize=figsize)
+    files = os.listdir(path)
+    
+    for num,file in enumerate(files,start=1):
+        img = Image.open(os.path.join(path,file))
+        if img.mode!='RGB':
+            img = img.convert('RGB')
+        
+        subplot(int(ceil(len(files)/K)),K,num)
+        show_image(img, file);
